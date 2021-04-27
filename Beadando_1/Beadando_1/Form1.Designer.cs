e7efd979bfd28e56770f7659f82ea107c75cad70
kovacsmark0517 <kovacs.mark.0517@gmail.com>
2021-04-27 23:41:21 +0200

    Add files via upload

---
+++ Beadando_1/Beadando_1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Beadando_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clb = new System.Windows.Forms.CheckedListBox();
+            this.tbBe = new System.Windows.Forms.TextBox();
+            this.tbTippek = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(1277, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Frissítés";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(1277, 409);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Számok ellenőrzése";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 12);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            this.dgv.Size = new System.Drawing.Size(1262, 753);
+            this.dgv.TabIndex = 3;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 780);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kezdő dátum";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1277, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Megjelenített oszlopok:";
+            // 
+            // clb
+            // 
+            this.clb.CheckOnClick = true;
+            this.clb.FormattingEnabled = true;
+            this.clb.Location = new System.Drawing.Point(1280, 29);
+            this.clb.Name = "clb";
+            this.clb.Size = new System.Drawing.Size(114, 244);
+            this.clb.TabIndex = 6;
+            // 
+            // tbBe
+            // 
+            this.tbBe.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.tbBe.Location = new System.Drawing.Point(1280, 338);
+            this.tbBe.Name = "tbBe";
+            this.tbBe.Size = new System.Drawing.Size(119, 20);
+            this.tbBe.TabIndex = 7;
+            this.tbBe.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbBe_KeyDown);
+            // 
+            // tbTippek
+            // 
+            this.tbTippek.Location = new System.Drawing.Point(1280, 383);
+            this.tbTippek.Name = "tbTippek";
+            this.tbTippek.ReadOnly = true;
+            this.tbTippek.Size = new System.Drawing.Size(119, 20);
+            this.tbTippek.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1277, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "1-90 közötti számot írj be majd \'Enter\'";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1483, 818);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbTippek);
+            this.Controls.Add(this.tbBe);
+            this.Controls.Add(this.clb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox clb;
+        private System.Windows.Forms.TextBox tbBe;
+        private System.Windows.Forms.TextBox tbTippek;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Beadando_1/Beadando_1/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Beadando_1
+{
+    public partial class Form1 : Form
+    {
+        //Az egész csv file soronként egy tömbben
+        string[] csvLines = File.ReadAllLines("lotto.csv");
+        string[,] LottoTomb = new string[4000, 16];
+        int lottoTombHeight;
+        int lottoTombWidth;
+        
+        string[] Tipp = new string[5];
+        int tippIndex = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lottoTombHeight = LottoTomb.GetLength(0);
+            lottoTombWidth = LottoTomb.GetLength(1);
+            this.dgv.ColumnCount = lottoTombWidth;
+
+            for (int i = 0; i < csvLines.Length; i++)
+            {
+                string[] rawData = csvLines[i].Split(',');
+                for (int j = 0; j < 16; j++)
+                {
+                    LottoTomb[i, j] = rawData[j];               
+                }              
+            }
+
+
+            DataGridViewRow row1 = new DataGridViewRow();
+            row1.CreateCells(this.dgv);
+
+
+            row1.Cells[0].Value = "Év";
+            row1.Cells[1].Value = "Hét";
+            row1.Cells[2].Value = "Húzás dátuma";
+            row1.Cells[3].Value = "5 találat db";
+            row1.Cells[4].Value = "5 találat Ft";
+            row1.Cells[5].Value = "4 találat db";
+            row1.Cells[6].Value = "4 találat Ft";
+            row1.Cells[7].Value = "3 találat db";
+            row1.Cells[8].Value = "3 találat Ft";
+            row1.Cells[9].Value = "2 találat db";
+            row1.Cells[10].Value = "2 találat Ft";
+            row1.Cells[11].Value = "1";
+            row1.Cells[12].Value = "2";
+            row1.Cells[13].Value = "3";
+            row1.Cells[14].Value = "4";
+            row1.Cells[15].Value = "5";
+            row1.Frozen = true;
+
+            this.dgv.Rows.Add(row1);
+
+
+            for (int x = 0; x < lottoTombHeight; x++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(this.dgv);
+                for (int y = 0; y < lottoTombWidth; y++)
+                {
+                    row.Cells[y].Value = LottoTomb[x, y];
+                    dgv.Columns[y].Visible = true;
+                }
+                this.dgv.Rows.Add(row);
+            }
+            clb.Items.Add("Év", CheckState.Checked);
+            clb.Items.Add("Hét", CheckState.Checked);
+            clb.Items.Add("Húzás dátuma", CheckState.Checked);
+            clb.Items.Add("5 találat db", CheckState.Checked);
+            clb.Items.Add("5 találat Ft", CheckState.Checked);
+            clb.Items.Add("4 találat db", CheckState.Checked);
+            clb.Items.Add("4 találat Ft", CheckState.Checked);
+            clb.Items.Add("3 találat db", CheckState.Checked);
+            clb.Items.Add("3 találat Ft", CheckState.Checked);
+            clb.Items.Add("2 találat db", CheckState.Checked);
+            clb.Items.Add("2 találat Ft", CheckState.Checked);
+            clb.Items.Add("1", CheckState.Checked);
+            clb.Items.Add("2", CheckState.Checked);
+            clb.Items.Add("3", CheckState.Checked);
+            clb.Items.Add("4", CheckState.Checked);
+            clb.Items.Add("5", CheckState.Checked);
+            
+
+
+            label1.Text = "Legfrissebb adat: " + LottoTomb[0, 2];           
+            label1.Refresh();
+
+        }
+        private void button1_Click(object sender, EventArgs e)//teljes kiírás
+        {
+            //label1.Text = clb.GetItemChecked(0).ToString();
+
+            for(int i = 0; i < 16; i++)
+            {
+                if (clb.GetItemChecked(i) == true)
+                {
+                    dgv.Columns[i].Visible = true;
+                }
+                else
+                {
+                    dgv.Columns[i].Visible = false;
+                }
+            }
+            dgv.Refresh();
+        }
+        //[0]Év [1]Hét [2]Húzás dátum [3]5 találat db [4]5 találat Ft [5]4 találat db [6]4 találat Ft [7]3 találat db [8]3 találat Ft [9]2 találat db [10]2 találat Ft [11]Sz1 [12]Sz2 [13]Sz3 [14]Sz4 [15]Sz5
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbBe_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                if (int.TryParse(tbBe.Text,out int n))
+                {
+                    if(n > 0 && n < 91)
+                    {
+                        if (Tipp.Contains(n.ToString()))
+                        {
+                            MessageBox.Show("Ezt a számot már írtad!");
+
+                        }
+                        else
+                        {
+                            
+                            tbTippek.Text += n.ToString() + " ";
+                            Tipp[tippIndex] = n.ToString();
+                            tippIndex++;
+                            tbBe.Text = "";
+                            if (tippIndex == 5)
+                            {
+                                tbBe.Text =Tipp[0] + " " + Tipp[1] + " " + Tipp[2] + " " + Tipp[3] + " " + Tipp[4] + " ";
+                                MessageBox.Show("Gratulálok mind az 5 számot beírtad!");
+                                tbBe.ReadOnly = true;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        tbBe.Text = "";
+                        MessageBox.Show("1 és 90 közötti számot adj meg!");
+                    }
+                }
+                else
+                {
+                    tbBe.Text = "";
+                    MessageBox.Show("Nem számot adtál meg!");
+                }
+            }
+        }
+    }
+        
+
+
+    }
+
